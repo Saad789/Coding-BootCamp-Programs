078abeb720cd274fc53a621dd36f9776511e9a2b
Mohammed Saad <34467824+Saad789@users.noreply.github.com>
2018-02-07 20:13:10 +0530

    Add files via upload

---
+++ Builder.java
@@ -0,0 +1,13 @@
+package JavaTutorial;
+
+public class Builder {
+
+	public static void main(String[] args) {
+		StringBuilder random = new StringBuilder(" Hello! ");
+		random.append("Have a good day!");
+		System.out.println(random);
+        
+		//System.out.println(sb.length());
+	}
+
+}
